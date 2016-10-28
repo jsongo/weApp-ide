181ea95c6f35689648e000127285bd2886e5b992
jason <jsongo@qq.com>
2016-10-28 14:32:52 +0800

    update to v0.10.102800

--- projectStores.js
+++ projectStores.js
@@ -7,61 +7,75 @@ function init() {
       o = void 0,
       i = void 0;
     if (0 === e.length) return t;
-    for (r = 0, i = e.length; r < i; r++) o = e.charCodeAt(r), t = (t << 5) - t + o,
-      t |= 0;
-    return t > 0 ? t : 0 - t;
+    for (r = 0, i = e.length; r < i; r++) o = e.charCodeAt(r), t = (t << 5) - t + o, t |= 0;
+    return t > 0 ? t : 0 - t
   }
 
   function t() {
-    var e = JSON.stringify(S);
-    localStorage.setItem("projectLists", e), i.writeFile(l, e);
+    var e = JSON.stringify(E);
+    localStorage.setItem("projectLists", e), n.writeFile(P, e)
   }
 
   function r(e, t) {
     if (t) {
       var r = e.projectpath,
-        c = o.join(__dirname, "../weapp/newquick/");
+        o = i.join(__dirname, "../weapp/newquick/");
       try {
-        var p = s.sync("./**/**", {
-          cwd: c
+        var p = a.sync("./**/**", {
+          cwd: o
         });
         p.forEach(function(e) {
-          var t = o.join(c, e),
-            n = o.join(r, e),
-            s = i.lstatSync(t);
-          if (s.isDirectory()) a.sync(n);
+          var t = i.join(o, e),
+            s = i.join(r, e),
+            a = n.lstatSync(t);
+          if (a.isDirectory()) c.sync(s);
           else {
-            var p = i.readFileSync(t);
-            i.writeFileSync(n, p);
+            var p = n.readFileSync(t);
+            n.writeFileSync(s, p)
           }
-        });
+        })
       } catch (f) {
-        n.error("projectStores.js initProject error " + f.toString());
+        s.error("projectStores.js initProject error " + f.toString())
       }
     }
   }
-  var o = require("path"),
-    i = require("fs"),
-    n = require("../common/log/log.js"),
-    s = require("glob"),
-    a = require("mkdir-p"),
-    c = require("../common/request/request.js"),
-    p = require("../config/urlConfig.js"),
-    f = require("../config/errcodeConfig.js"),
-    u = require("../weapp/commit/unpack.js"),
-    j = require("../config/dirConfig.js"),
-    d = require("events").EventEmitter,
-    g = j.WeappProjectInfo;
-  if (!g) {
-    var h = o.join(nw.App.getDataPath(), "..");
-    g = o.join(h, "WeappProject");
+
+  function o(e) {
+    var t = arguments.length > 1 && void 0 !== arguments[1] ? arguments[1] : function() {},
+      r = require("../actions/windowActions.js"),
+      o = "alert";
+    r.showConfirm({
+      content: e,
+      type: o,
+      callback: t
+    })
   }
-  var l = o.join(g, "projectinfo.json"),
-    S = JSON.parse(localStorage.getItem("projectLists")) || [];
-  i.writeFile(l, localStorage.getItem("projectLists") || "[]");
-  var v = {},
-    m = null,
-    y = !1,
+  var i = require("path"),
+    n = require("fs"),
+    s = require("../common/log/log.js"),
+    a = require("glob"),
+    c = require("mkdir-p"),
+    p = require("../common/request/request.js"),
+    f = require("../config/urlConfig.js"),
+    u = require("../config/errcodeConfig.js"),
+    j = (require("../weapp/commit/unpack.js"), require("../config/dirConfig.js")),
+    d = require("events").EventEmitter,
+    g = "last-select-project",
+    h = require("../config/config.js"),
+    l = h.SELECT_URL_TYPE,
+    S = h.SELECT_UNKNOW_TYPE,
+    v = j.WeappProjectInfo;
+  if (!v) {
+    var m = i.join(nw.App.getDataPath(), "..");
+    v = i.join(m, "WeappProject")
+  }
+  var P = i.join(v, "projectinfo.json"),
+    E = JSON.parse(localStorage.getItem("projectLists")) || [];
+  n.writeFile(P, localStorage.getItem("projectLists") || "[]");
+  var w = {},
+    y = null,
+    _ = !1,
+    C = parseInt(localStorage.getItem(g)) || S,
     O = {
       Network: {
         RequestDomain: [],
@@ -79,121 +93,131 @@ function init() {
         MaxDownloadConcurrent: 5
       }
     };
-  S.forEach(function(t) {
-    t.hash = e(t.projectid), void 0 === t.es6 && (t.es6 = !0);
+  E.forEach(function(t) {
+    t.hash = e(t.projectid), void 0 === t.es6 && (t.es6 = !0), void 0 === t.watcher && (t.watcher = !0), void 0 === t.editWebview && (t.editWebview = !0)
   });
-  var P = Object.assign({}, d.prototype, {
+  var q = Object.assign({}, d.prototype, {
+    getLastSelect: function() {
+      if (C === l || C === S) return parseInt(C);
+      var e = this.getProjectByHash(C);
+      if (e) {
+        var t = "projectattr" + e.hash,
+          r = JSON.parse(localStorage.getItem(t)),
+          o = e.isTourist;
+        if (r || o) return q.setProjectConfig(e, function() {}), e
+      }
+      return S
+    },
+    setProjectType: function(e) {
+      C = e, localStorage.setItem(g, e)
+    },
     getCurrentProject: function() {
-      return m;
+      return y
     },
     setProjectEs6: function(e, r) {
       var o = this.getProjectByHash(e);
-      o.es6 = r, t(), this.emit("PROJECT_STORES_CHANGE", o, r);
+      o.es6 = r, t(), this.emit("PROJECT_STORES_CHANGE_ES6", o, r)
     },
     setProjectMinified: function(e, r) {
       var o = this.getProjectByHash(e);
-      o.minified = r, t();
+      o.minified = r, t()
+    },
+    setProjectEditWebview: function(e, r) {
+      var o = this.getProjectByHash(e);
+      o.editWebview = r, t(), this.emit("PROJECT_STORES_CHANGE_EDIT_WEBVIEW", o, r)
+    },
+    setProjectWatcher: function(e, r) {
+      var o = this.getProjectByHash(e);
+      o.watcher = r, t()
     },
     getProjectByHash: function(e) {
-      return e = parseInt(e), S.find(function(t) {
-        return t.hash === e;
-      });
+      return e = parseInt(e), E.find(function(t) {
+        return t.hash === e
+      })
     },
     getProjectByID: function(e) {
-      return S.find(function(t) {
-        return t.projectid === e;
-      });
+      return E.find(function(t) {
+        return t.projectid === e
+      })
     },
     getProjectList: function() {
-      return n.info("projectStores.js getProjectList " + JSON.stringify(S)), S;
+      return s.info("projectStores.js getProjectList " + JSON.stringify(E)), E
     },
-    addVerifyProject: function(r, i) {
-      var s = this;
-      r.hash = e(r.projectid);
-      var a = r.projectpath;
-      u(o.join(a, "source.wx"), function(e, o) {
-        e ? i(e) : (r.projectpath = o, r.isVerify = !1, S.unshift(r), t(), n.info("projectStores.js add " + JSON.stringify(r)),
-          s.emit("ADD_PROJECT", S), i(null, o));
-      });
-    },
+    addVerifyProject: function(e, t) {},
     add: function(o, i) {
-      o.hash = e(o.projectid), o.es6 = !0, S.unshift(o), r(o, i), t(), n.info("projectStores.js add " + JSON.stringify(o)),
-        this.emit("ADD_PROJECT", S);
+      o.hash = e(o.projectid), o.es6 = !0, o.watcher = !0, o.editWebview = !0, E.unshift(o), r(o, i), t(), s.info("projectStores.js add " + JSON.stringify(o)), this.emit("ADD_PROJECT", E)
     },
     del: function(e) {
-      var r = S.findIndex(function(t) {
-        return t.projectid === e;
+      var r = E.findIndex(function(t) {
+        return t.projectid === e
       });
       if (r > -1) {
-        var o = S[r];
-        delete localStorage["projectattr" + o.hash], S.splice(r, 1), t(), n.info("projectStores.js del " + e),
-          this.emit("DEL_PROJECT", S);
+        var o = E[r];
+        delete localStorage["projectattr" + o.hash], E.splice(r, 1), t(), s.info("projectStores.js del " + e), this.emit("DEL_PROJECT", E)
       }
     },
     close: function() {
-      this.emit("CLOSE_PROJECT");
+      this.emit("CLOSE_PROJECT")
     },
     restart: function(e) {
-      this.emit("RESTART_PROJECT", e);
+      this.emit("RESTART_PROJECT", e)
     },
     getProjectConfig: function(e) {
-      return v[e.hash];
+      return w[e.hash]
     },
     setProjectConfig: function(e, t) {
-      if (!y) {
-        m = e;
+      if (!_) {
+        y = e;
         var r = "projectattr" + e.hash,
-          o = JSON.parse(localStorage.getItem(r)); //, i = e.isTourist;
-        //if (i) {
-        //var s = Object.assign({}, O);
-        //return s.appid = e.appid, v[e.hash] = s, void t();
-        //}
-        y = !0, o && (v[e.hash] = o, t());
-        var a = p.getWeappAttrURL,
-          u = a + "?appid=" + e.appid + "&_r=" + Math.random();
-        n.info("projectStores.js begin get projectAttr " + u), c({
-          url: u,
+          i = JSON.parse(localStorage.getItem(r));
+        // n = e.isTourist;
+        /*if (n) {
+          var a = Object.assign({}, O);
+          return a.appid = e.appid, w[e.hash] = a, void t()
+        }*/
+        _ = !0, i && (w[e.hash] = i, t());
+        var c = f.getWeappAttrURL,
+          j = c + "?appid=" + e.appid + "&_r=" + Math.random();
+        s.info("projectStores.js begin get projectAttr " + j), p({
+          url: j,
           body: JSON.stringify({
             appid_list: [e.appid]
           }),
           method: "post",
           needToken: 1
-        }, function(i, s, a) {
-          if (y = !1, i) return n.error("projectStores.js end get projectAttr network error: " + JSON.stringify(i)),
-            void alert(JSON.stringify(i));
-          n.info("projectStores.js end get projectAttr " + a);
-          /*var c = void 0;
-          try {
-              c = JSON.parse(a);
-          } catch (p) {
-              return n.error("projectStores.js end get projectAttr parse body error: " + a + " " + JSON.stringify(i)), 
-              void (!o && alert("系统错误 " + a));
-          }
-          var u = c.baseresponse, j = u ? parseInt(u.errcode) :0;*/
-          //if (0 === j) {
+        }, function(n, a, c) {
+          _ = !1;
+          require("../actions/windowActions.js");
+          if (n) return s.error("projectStores.js end get projectAttr network error: " + JSON.stringify(n)), void(!i && o(JSON.stringify(n)));
+          s.info("projectStores.js end get projectAttr " + c);
+          var p = void 0;
+          /*try {
+            p = JSON.parse(c)
+          } catch (f) {
+            return s.error("projectStores.js end get projectAttr parse body error: " + c + " " + JSON.stringify(n)), void(!i && o("系统错误 " + c))
+          }*/
+          // var j = p.baseresponse,
+          //   d = j ? parseInt(j.errcode) : 0;
+          // if (0 === d) {
           if (true) {
-            //var d = c.attr_list[0];
-            var d = {
+            // var g = p.attr_list[0];
+            var g = {
               Setting: {
                 MaxLocalstorageSize: 10
               }
             };
-            v[e.hash] = d, localStorage.setItem(r, JSON.stringify(d)), o || t();
+            w[e.hash] = g, localStorage.setItem(r, JSON.stringify(g)), i || t()
           } else {
-            if (j === f.DEV_App_Not_Band) {
-              alert("当前开发者未绑定此 appid ，请到 mp 后台操作后重试"), nw.Shell.openExternal("https://mp.weixin.qq.com/"),
-                n.error("projectStores.js setProjectConfig error " + j);
-              var g = require("./webviewStores.js");
-              return void g.emit("NOT_LOGIN");
-            }!o && alert("系统错误 " + a);
+            if (d === u.DEV_App_Not_Band) return o("当前开发者未绑定此 appid ，请到 mp 后台操作后重试", function() {
+              nw.Shell.openExternal("https://mp.weixin.qq.com/")
+            }), void s.error("projectStores.js setProjectConfig error " + d);
+            !i && o("系统错误 " + c)
           }
-        });
+        })
       }
     }
   });
-  _exports = P;
+  _exports = q
 }
-
 var _exports;
-
 init(), module.exports = _exports;
